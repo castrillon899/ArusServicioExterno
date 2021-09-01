e6d12a019b084a321b96b09f6d1c8c5352c70b21
Angel Ernesto Marin Castrillon - Ceiba Software <angel.marin@ceiba.com.co>
2021-08-31 19:50:52 -0500

    actaulizacin

--- ADN-UsuariosBackend/java-arquitectura-hexagonal/microservicio/infraestructura/src/main/java/com/ceiba/usuario/adaptador/repositorio/RepositorioUsuarioMysql.java
+++ ADN-UsuariosBackend/java-arquitectura-hexagonal/microservicio/infraestructura/src/main/java/com/ceiba/usuario/adaptador/repositorio/RepositorioUsuarioMysql.java
@@ -16,21 +16,13 @@ public class RepositorioUsuarioMysql implements RepositorioUsuario {
 	@SqlStatement(namespace = "usuario", value = "crear")
 	private static String sqlCrear;
 
-	@SqlStatement(namespace = "usuario", value = "actualizar")
-	private static String sqlActualizar;
-
-	@SqlStatement(namespace = "usuario", value = "eliminar")
-	private static String sqlEliminar;
+	
+	
 
 	@SqlStatement(namespace = "usuario", value = "existe")
 	private static String sqlExiste;
 
-	@SqlStatement(namespace = "usuario", value = "existeExcluyendoId")
-	private static String sqlExisteExcluyendoId;
-
-	@SqlStatement(namespace = "usuario", value = "buscarUsuarioParaGestionDeSolicitud")
-	private static String sqlBuscarUsuarioParaGestionDeLaSolicitud;
-
+	
 
 	
 	public RepositorioUsuarioMysql(CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate) {
@@ -42,13 +34,7 @@ public class RepositorioUsuarioMysql implements RepositorioUsuario {
 		return this.customNamedParameterJdbcTemplate.crear(usuario, sqlCrear);
 	}
 
-	@Override
-	public void eliminar(Long id) {
-		MapSqlParameterSource paramSource = new MapSqlParameterSource();
-		paramSource.addValue("id", id);
-
-		this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().update(sqlEliminar, paramSource);
-	}
+	
 
 	@Override
 	public boolean existe(String nombre) {
@@ -61,28 +47,33 @@ public class RepositorioUsuarioMysql implements RepositorioUsuario {
 
 	@Override
 	public void actualizar(Usuario usuario) {
-		this.customNamedParameterJdbcTemplate.actualizar(usuario, sqlActualizar);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminar(Long id) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public boolean existeExcluyendoId(Long id, String nombre) {
-		MapSqlParameterSource paramSource = new MapSqlParameterSource();
-		paramSource.addValue("id", id);
-		paramSource.addValue("nombre", nombre);
-
-		return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate()
-				.queryForObject(sqlExisteExcluyendoId, paramSource, Boolean.class);
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public Usuario buscarUsuarioParaGestionDeLaSolicitud(String tipoSolicitud) {
-		MapSqlParameterSource paramSource = new MapSqlParameterSource();
-		paramSource.addValue("tipoSolicitud", tipoSolicitud);
-		 return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate()
-				.queryForObject(sqlBuscarUsuarioParaGestionDeLaSolicitud, paramSource, new MapeoUsuario());
-		
-
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+
+
+
+
+
 
 
 }
